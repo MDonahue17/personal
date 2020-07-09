8d1008d45d908c6163e7a6208801b6e48d348314
Michael Donahue <michaeldonahue17@gmail.com>
2020-07-08 21:50:26 -0400

    Updates

--- precache-manifest.bf000a72cb19cbb85b31c010bc61d83f.js
+++ precache-manifest.bf000a72cb19cbb85b31c010bc61d83f.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ccdae771dd38d5a0c03b36b64b021905",
+    "revision": "8842632c1c4f65786e9efa5b1220cabb",
     "url": "/index.html"
   },
   {
-    "revision": "b78304a9407ebd0f826d",
+    "revision": "58ba766fa3cfec594782",
     "url": "/static/css/2.de424728.chunk.css"
   },
   {
-    "revision": "3e0fea7a5e5e2909096a",
-    "url": "/static/css/main.7c4e4f60.chunk.css"
+    "revision": "b7c4e8f936153ad02363",
+    "url": "/static/css/main.b5fb0dc4.chunk.css"
   },
   {
-    "revision": "b78304a9407ebd0f826d",
-    "url": "/static/js/2.7f9c6297.chunk.js"
+    "revision": "58ba766fa3cfec594782",
+    "url": "/static/js/2.8494d0a4.chunk.js"
   },
   {
     "revision": "928d7b5eb39d16fe9a880722c974b51e",
-    "url": "/static/js/2.7f9c6297.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.8494d0a4.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3e0fea7a5e5e2909096a",
-    "url": "/static/js/main.13039ccc.chunk.js"
+    "revision": "b7c4e8f936153ad02363",
+    "url": "/static/js/main.9bab88f2.chunk.js"
   },
   {
     "revision": "4e298d7ae7623fec8b7e",
@@ -36,14 +36,6 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/media/Donahue_Michael_Resume.65cc7a50.pdf"
   },
   {
-    "revision": "366ef251026efc407a256d9a36f2a6c7",
-    "url": "/static/media/Jon-Ross-Joon-14.366ef251.jpg"
-  },
-  {
-    "revision": "acb5cb79d4a8de9e31b5b6dc5aa7976c",
-    "url": "/static/media/Jon-Ross-Joon-6.acb5cb79.jpg"
-  },
-  {
     "revision": "6b3803e4cb04c1ae04315acca0822a1b",
     "url": "/static/media/Tyler.6b3803e4.JPG"
   },
@@ -52,24 +44,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/media/bike.0b8a1a91.jpg"
   },
   {
-    "revision": "b2b5b8e40350ce3408722d1574764f8b",
-    "url": "/static/media/cj-3.b2b5b8e4.jpg"
-  },
-  {
-    "revision": "e29f6c8db8689153d6f7dd0806ddfd4c",
-    "url": "/static/media/cj-5.e29f6c8d.jpg"
-  },
-  {
     "revision": "fc2cecb61089b8af512514fe87f27ab6",
     "url": "/static/media/clothing.fc2cecb6.jpg"
   },
   {
     "revision": "50b941d2f045a41f75be627511df766c",
     "url": "/static/media/growlers.50b941d2.jpg"
-  },
-  {
-    "revision": "83f8cf6d587e7ae774546d6e52a4b949",
-    "url": "/static/media/jr-3.83f8cf6d.jpg"
   },
   {
     "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
@@ -82,14 +62,6 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "004d65a001c1df9a08a2f9a9d2f5a1ff",
     "url": "/static/media/stories-1.004d65a0.jpg"
-  },
-  {
-    "revision": "315d35d443c43dd5c5b5afcd0672a994",
-    "url": "/static/media/wash-mon-1.315d35d4.jpg"
-  },
-  {
-    "revision": "0f24e6ddd0be75d43f9af9cc55297141",
-    "url": "/static/media/woman-1.0f24e6dd.jpg"
   },
   {
     "revision": "4606d3282b465bc10595ccce75d03606",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.66a189f7d32acdbe27cf763250d1c25a.js"
+  "/precache-manifest.bf000a72cb19cbb85b31c010bc61d83f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
